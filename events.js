ad4261c8841ac8a675edd1d9d8e72207fb7f8968
KarenE <karegvcs@gamil.com>
2015-07-16 14:29:46 -0500

    actualizacion de interfaz

--- events.js
+++ events.js
@@ -6,9 +6,35 @@
 					$('.datos').val("");
 					$('input:radio[name="genero"]').prop("checked", false);
 				});*/
-				$('#iniciar').click(function(){
+				/*$('#iniciar').click(function(){
+					$('#barra').click(function(){
+						$('#barra li').css("background-color", 'red')
+					});
 					$('#campo').html("");
 					$('#campo').load('login.html');
+					$
 					
 				});
+			});*/
+			
+			$('#iniciar').click(function(){
+				$('#barra li #iniciar' ).css('background', 'green')
+					$('#login').animate({
+							right: '250px',
+					})
+						$('#campo').load('login.html');
+			
 			});
+			$('#base').click(function(){});
+			
+			$('#codigo').keyup(function(e){
+				if(e.keyCode == 13)
+				{
+					var text= prompt("TEMA", "Ingrese el tema");
+				}
+			});
+		});
+		
+			
+			
+			
